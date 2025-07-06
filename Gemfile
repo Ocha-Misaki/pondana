23a8616cb00b7736a8e495d5bba3f30ccc23891b
Ocha-Misaki <himawari.158fuji@gmail.com>
2025-07-06 18:19:00 +0900

    rakuten apiを使って書籍検索できるように

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "cssbundling-rails"
 gem "devise"
 gem 'dotenv-rails'
 gem 'haml-rails'
+gem "image_processing"
 gem "jbuilder"
 gem "jsbundling-rails"
 gem "kamal", require: false
@@ -15,6 +16,7 @@ gem "kaminari"
 gem "pg"
 gem "propshaft"
 gem "puma"
+gem 'rakuten_web_service'
 gem 'simple_form'
 gem "solid_cable"
 gem "solid_cache"
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.default_order.page(params[:page])
+    @form = BookSearchForm.new(book_search_form_params)
+    @books = @form.call.default_order.page(params[:page])
+  end
+
+  private
+
+  def book_search_form_params
+    params.fetch(:book_search_form, {}).permit(:query)
   end
 end
